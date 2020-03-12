cf3ba88a9538f458a656131cd81481fdbf3f5b63
Michael George <georgem@wlwv.k12.or.us>
2020-03-12 09:53:48 -0700

    started cauliflower

---
+++ src/Cauliflower.java
@@ -0,0 +1,28 @@
+public class Cauliflower extends Vegetable{
+    public Cauliflower(){
+        super("Cauliflower", "Cruciferous");
+        String str = "Plant seeds about 1/4-inch deep+\n";
+        str += "Water one to two inches per week+\n";
+        str += "Fertilize after first fruit set+\n";
+        str += "Harvest as soon as peppers reach desired size or color+\n";
+        str += "Use a sharp knife or scissors to cut peppers clean off the plant+\n";
+        setInstructions(str);
+    }
+    public String feed(){
+        return "Mmm! Thank you, kind sir";
+    }
+    public String water(){
+        return "Slurp, slurp, schlup!";
+    }
+    public String harvest(){
+        return "Wait... where's the water? What are you doing with that— AAAAHH!";
+    }
+
+    public static void main(String[] args) {
+        BellPepper bp = new BellPepper();
+        System.out.println(bp.getInstructions());
+        System.out.println(bp.feed());
+        System.out.println(bp.water());
+        System.out.println(bp.harvest());
+    }
+}
